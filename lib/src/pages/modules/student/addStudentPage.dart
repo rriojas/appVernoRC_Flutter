92f1d2f354df2d4297e5868fa12db15f9c0983a2
Rene Ramos <rramos@serfim.mx>
2022-06-17 02:40:44 -0500

    clean code

--- lib/src/pages/modules/student/addStudentPage.dart
+++ lib/src/pages/modules/student/addStudentPage.dart
@@ -12,83 +12,69 @@ class AddStudentPage extends StatelessWidget {
   AddStudentPage({Key? key}) : super(key: key);
 
   late GlobalKey<FormState> _formKey;
-
   late FormGroup fg;
-
   Map<String, TextEditingController> textEditingControllers = {};
-
   var allWidgets = <Widget>[];
-
-  @override
   Map<String, FormControl> mapReactiveForm = {};
-
-  List keys = [];
+  String keys = '';
 
   @override
   Widget build(BuildContext context) {
-    keys.clear();
     mapReactiveForm.clear();
     allWidgets.clear();
     textEditingControllers.clear();
 
-    for (var element in listTextFieldMap) {
-      keys.add(element.keys.toString().replaceAll('(', '').replaceAll(')', ''));
-    }
-
     for (int i = 0; i < listTextFieldMap.length; i++) {
-      final validators = listTextFieldMap[i][keys[i]]?['validators']
+      keys = listTextFieldMap[i]
+          .keys
+          .toString()
+          .replaceAll('(', '')
+          .replaceAll(')', '');
+      final validators = listTextFieldMap[i][keys]?['validators']
           ?.map((e) =>
               e as Map<String, dynamic>? Function(AbstractControl<dynamic>))
           .toList();
 
-      final inputFormatted = listTextFieldMap[i][keys[i]]?['inputFormatted']
+      final inputFormatted = listTextFieldMap[i][keys]?['inputFormatted']
           ?.map((e) => e as TextInputFormatter)
           .toList();
 
       FormControl<String> item = FormControl(validators: validators!);
 
       var textEdit = TextEditingController();
+      final type =
+          listTextFieldMap[i][keys]?['type']?.map((e) => e as String).toList();
 
-      if (keys[i] == 'Escuela/Facultad/Centro de investigación') {
-        allWidgets.add(
-          MyDropdown(
+      switch (type?[0]) {
+        case 'text':
+          allWidgets.add(CreateMyTextFormField(
             controller: textEdit,
-            label: keys[i],
-            stream: service.getInstitutionStream,
-          ),
-        );
-      } else if (keys[i] == 'Institución') {
-        allWidgets.add(
-          MyDropdown(
+            label: keys,
+            fcn: keys,
+            inputFormated: inputFormatted,
+          ));
+          break;
+        case 'drop':
+          allWidgets.add(
+            MyDropdown(
+              controller: textEdit,
+              label: keys,
+              stream: service.getInstitutionStream,
+            ),
+          );
+          break;
+        case 'birth':
+          allWidgets.add(MyTextFormBirthday(
             controller: textEdit,
-            label: keys[i],
-            stream: service.getInstitutionStream,
-          ),
-        );
-      } else if (keys[i] == 'Genero') {
-        allWidgets.add(
-          MyDropdown(
-            controller: textEdit,
-            label: keys[i],
-            stream: service.getInstitutionStream,
-          ),
-        );
-      } else if (keys[i] == 'Fecha de nacimiento') {
-        allWidgets.add(MyTextFormBirthday(
-          controller: textEdit,
-          label: keys[i],
-        ));
-      } else {
-        allWidgets.add(CreateMyTextFormField(
-          controller: textEdit,
-          label: keys[i],
-          fcn: keys[i],
-          inputFormated: inputFormatted,
-        ));
+            label: keys,
+          ));
+          break;
+        default:
       }
-      mapReactiveForm.putIfAbsent(keys[i], () => item);
 
-      textEditingControllers.putIfAbsent(keys[i], () => textEdit);
+      mapReactiveForm.putIfAbsent(keys, () => item);
+
+      textEditingControllers.putIfAbsent(keys, () => textEdit);
     }
 
     allWidgets.addAll([
@@ -103,7 +89,9 @@ class AddStudentPage extends StatelessWidget {
         ),
       ),
       TextButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.pop(context);
+        },
         child: const Text(
           'Cancelar',
         ),
@@ -143,11 +131,6 @@ class AddStudentPage extends StatelessWidget {
 
   validateFormStudent() {
     fg.markAllAsTouched();
-
-    textEditingControllers.forEach((key, value) {
-      print(value.text);
-    });
-
     if (_formKey.currentState != null && !_formKey.currentState!.validate()) {
       return;
     }
--- lib/src/pages/modules/student/settingsStudent.dart
+++ lib/src/pages/modules/student/settingsStudent.dart
@@ -2,45 +2,52 @@ import 'package:flutter/services.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
-final listTextFieldMap = [
+List<Map<String, Map<String, List<dynamic>>>> listTextFieldMap = [
   {
     'Nombre': {
+      'type': ['text'],
       'validators': [Validators.required, Validators.minLength(3)],
       'inputFormatted': []
     }
   },
   {
     'Apellido paterno': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Apellido materno': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Calle': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Numero': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Colonia': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Codígo postal': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': [
         (FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))),
@@ -50,18 +57,21 @@ final listTextFieldMap = [
   },
   {
     'Fecha de nacimiento': {
+      'type': ['birth'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Genero': {
+      'type': ['drop'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Telefono': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': [
         FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
@@ -72,30 +82,35 @@ final listTextFieldMap = [
   },
   {
     'Institución': {
+      'type': ['drop'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Escuela/Facultad/Centro de investigación': {
+      'type': ['drop'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Correo': {
+      'type': ['text'],
       'validators': [Validators.required, Validators.email],
       'inputFormatted': []
     }
   },
   {
     'Contraseña': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
   },
   {
     'Confirma contraseña': {
+      'type': ['text'],
       'validators': [Validators.required],
       'inputFormatted': []
     }
